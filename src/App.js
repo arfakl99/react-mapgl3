7ada92cfca04f08393441a3705925d3fd00fefcd
arfakl99 <muhammadarifnust99@gmail.com>
2023-01-10 14:10:15 +0500

    reat-map-gl with marker

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 import * as React from 'react';
 import {Map,Marker} from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+// import icon from '../public/imgs/office.png';
+ import icon from './imgs/office.png';
+ import { useState } from 'react';
 
 function App() {
+  
   return (
     <div className="App">
       <Map
@@ -12,7 +16,7 @@ function App() {
         {
           longitude:73.089187,
           latitude:33.560784,
-          zoom: 14
+          zoom: 10
         }
       } 
       style={
@@ -23,8 +27,13 @@ function App() {
       }
       mapStyle="mapbox://styles/mapbox/streets-v9"
       mapboxAccessToken='pk.eyJ1IjoibXRheXlhYm1pciIsImEiOiJjbDZuaWQ5Z3QwMTNyM2tsbGs2emtlc21uIn0.cwMj_2X3NoUdcbWnLGiG4g'
-      />
-
+      >
+        <Marker longitude={73.090830} latitude={33.560788} anchor="bottom" >
+      <img src={icon} />
+      
+    </Marker>
+    
+ </Map>
     </div>
   );
 }
