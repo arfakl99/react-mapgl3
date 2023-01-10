4e1a9e1eab5217977b55b8f4ced007395b8da29d
arfakl99 <muhammadarifnust99@gmail.com>
2023-01-10 12:56:59 +0500

    set map view

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import * as React from 'react';
+import {Map,Marker} from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map
+      initialViewState={
+        {
+          longitude:73.089187,
+          latitude:33.560784,
+          zoom: 14
+        }
+      } 
+      style={
+        {
+          width:600,
+          height:400
+        }
+      }
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxAccessToken='pk.eyJ1IjoibXRheXlhYm1pciIsImEiOiJjbDZuaWQ5Z3QwMTNyM2tsbGs2emtlc21uIn0.cwMj_2X3NoUdcbWnLGiG4g'
+      />
+
     </div>
   );
 }
